37de16d7bccd4c349b839206494ab822664aa4c3
Vu pt <vuit6789@gmail.com>
2018-09-20 22:34:05 +0700

    Chapter 14: Following users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = t "users_controller.followings"
+    @user  = User.find_by id: params[:id]
+    @users = @user.following.paginate(page: params[:page])
+    render "show_follow"
+  end
+
+  def followers
+    @title = t "users_controller.followers"
+    @user  = User.find_by id: params[:id]
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
+  end
+
   private
 
   def user_params
